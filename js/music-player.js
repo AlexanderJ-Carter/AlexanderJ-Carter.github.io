22d7b336decff209b30183b30a4bb0cc801d0dd1
Alex-hwang <2253940186@qq.com>
2024-10-17 13:40:35 +0800

    优化名称

--- js/music-player.js
+++ js/music-player.js
@@ -8,18 +8,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const nextSongButton = document.getElementById("next-song");
 
   const songs = [
-    { title: "曾斌斌 - 冬忆", src: "music/曾斌斌 - 冬忆 (Inst_).flac" },
+    { title: "曾斌斌 - 冬忆", src: "music/冬忆.flac" },
     {
       title: "久石让 - 天空の城ラピュタ",
-      src: "music/久石让 - 天空の城ラピュタ (天空之城).flac",
+      src: "music/天空之城.flac",
     },
     {
       title: "中央交响乐团 - 彩云追月",
-      src: "music/中央交响乐团 - 彩云追月.ogg",
+      src: "music/彩云追月.ogg",
     },
     {
       title: "海佳 - 雨的印记",
-      src: "music/海佳 - 雨的印记.ogg",
+      src: "music/雨的印记.ogg",
     },
   ];
   let currentSongIndex = 0;
